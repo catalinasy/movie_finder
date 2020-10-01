c3ff3fc1dfcaa4c6a21dec7c6784a5e2e654741c
Catalina Syddall <catalinasyddall@gmail.com>
2020-10-01 16:51:54 -0300

    fix linter && adds env example

--- src/Containers/SearchPage.jsx
+++ src/Containers/SearchPage.jsx
@@ -5,6 +5,10 @@ import Results from '../Components/Results';
 const SearchPage = () => {
     const results = [{id: 1, name: 'nombree'}, {id: 2, name: 'otra peli'}];
 
+    console.log(process.env.REACT_APP_API_KEY, 'aca esta??')
+
+    //https://api.themoviedb.org/3/movie/550?api_key=8041adf27e988990a42d4a523a5f3e5f
+
 
   return (
     <>
